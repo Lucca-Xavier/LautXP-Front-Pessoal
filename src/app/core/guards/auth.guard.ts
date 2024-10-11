bd36e26df3f5071f3b21463f6a92b5f27fd037fd
Kbraga27 <160952244+Kbraga27@users.noreply.github.com>
2024-10-11 16:30:21 -0300

    publicidade front

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -9,10 +9,10 @@ export class AuthGuard {
   }
   canActivate() {
     const token = this.authService.getToken;
-
+    //return true
     //Check if the token is expired or not and if token is expired then redirect to login page and return false
     if (token && !this.jwtHelper.isTokenExpired(token.token)) {
-      return true;
+     return true;
     }
 
     this.router.navigate(["/login"]);
