825d09439be22f21b062f1abf2b4a6ebc6cd60c7
Arthur Viotti <arthurviottidev@gmail.com>
2024-10-21 09:07:09 -0300

    atualização da pagina externa do dashboard

--- src/app/module/dashboard/dashboard/dashboard.component.ts
+++ src/app/module/dashboard/dashboard/dashboard.component.ts
@@ -5,46 +5,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  colors = {
-    label: 'My dataset',
-    backgroundColor: 'rgba(77,189,116,.2)',
-    borderColor: '#4dbd74',
-    pointHoverBackgroundColor: '#fff',
-  }
-
-  labels = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-
-  data = {
-    labels: this.labels,
-    datasets: [{
-      data: [65, 59, 84, 84, 51, 55, 40],
-      ...this.colors,
-      fill: { value: 65 }
-    }],
-  }
-
-  options = {
-    plugins: {
-      legend: {
-        display: false
-      }
-    },
-    maintainAspectRatio: false,
-    elements: {
-      line: {
-        tension: 0.4
-      },
-    }
-  };
-
-  githubData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'GitHub Commits',
-        backgroundColor: '#f87979',
-        data: [40, 20, 12, 39, 10, 80, 40]
-      }
-    ]
-  };
+  
 }
